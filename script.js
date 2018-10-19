c2aa06666464202d0617217a5a5cf238cdea08a8
Ruben Ponce <ruben_ponce1@yahoo.com>
2018-10-18 17:08:11 -0700

    updated array of quotes

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 // FSJS - Random Quote Generator
 
 // Create the array of quote objects and name it quotes
-//quotes[quote, author]
+//quotes[quote, source]
 var quotes = [
-    {quote: "Unfortunately what is little recognized is that the most worthwhile scientific books are those in which the author clearly indicates what he does not know; for an author most hurts his readers by concealing difficulties",
-    author: "Evariste Galois",
-    citation: "May 31, 1832",
-    year: "" },
+    {quote: "Unfortunately what is little recognized is that the most worthwhile scientific books are those in which the source clearly indicates what he does not know; for an source most hurts his readers by concealing difficulties",
+    source: "Evariste Galois",
+    citation: "The Last Letter of Galois",
+    year: "1832" },
 
     {quote:"Have you ever had a dream, Neo, that you were so sure was real? What if you were unable to wake from that dream? How would you know the difference between the dream world and the real world?",
-    author: "Morpheus",
+    source: "Morpheus",
     citation:"The Matrix",
-    year: "March 31, 1999"},
+    year: "1999"},
 
     {quote: "You must do the things you think you cannot do.",
-    author: "Eleanor Roosevelt",
+    source: "Eleanor Roosevelt",
     citation:"The Washington Post",
-    year:"June 27 1956"}
+    year:"1956"}
 ];
 
 
@@ -24,14 +24,13 @@ var quotes = [
 
 let quoteBox = document.querySelector(".quote");
 let sources = document.querySelector(".source");
-let citation = document.querySelector(".citation");
-let year = document.querySelector(".year");
+let citations = document.querySelector(".citation");
+let years = document.querySelector(".year");
 let button = document.querySelectorAll('button');
 
 function backGroundColorChange()
-  {var randomNumber = ((Math.random()+2)*16777216|0).toString(16).slice(1)
+  {var randomNumber = ((Math.random()+2)*16777216|0).toString(16).slice(1);
     document.body.style.backgroundColor = "#"+randomNumber;
-    button.style.backgroundColor = "#"+randomNumber;
   }
 //standard random Number generator in Javascript, but range of number is between 0 and length of array
 
@@ -40,11 +39,20 @@ function getRandomQuote(){
   console.log(randomNumber);
   return randomNumber;
 }
-// Create the printQuote function
+
 //quoteBox represents the paragraph, and sources represents the source.
 function printQuote(){
+  let source = quotes[quoteNum].source;
+  let citation = quotes[quoteNum].citation;
+  let year = quotes[quoteNum].year;
   var quoteNum = getRandomQuote();
-quoteBox.innerHTML = quotes[quoteNum].quote
+quoteBox.innerHTML = quotes[quoteNum].quote;
+if(quotes[quoteNum].citation=== undefined){
+  return 0;
+} if(quotes[quoteNum].year===undefined){
+  return 0;
+}
+
 
 backGroundColorChange();
 }
